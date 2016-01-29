9c22fd8230f3872fe02164915c2fda6599beffaa
windfant <wind_fant@yahoo.com>
2016-01-29 21:42:38 +0800

    commit2

--- server.R
+++ server.R
@@ -2,16 +2,23 @@ diabetesRisk <- function(glucose) glucose / 200
 
 data <- mtcars
 
+desc <- c("Miles/(US) gallon", "Number of cylinders", "Displacement (cu.in.)", "Gross horsepower", "Rear axle ratio", "Weight (lb/1000)",	"1/4 mile time", "V/S", "Transmission (0 = automatic, 1 = manual)","Number of forward gears", "Number of carburetors")
+
+description <- data.frame(names(data), desc)
+names(description)[1] <- "var"
+
 shinyServer(
   
   function(input, output) {
-    output$plot1 <- renderPlot({hist(data[,input$attribute1], main=input$attribute1, xlab=input$attribute1, ylab="count")})
-    output$plot2 <- renderPlot({hist(data[,input$attribute2], main=input$attribute2, xlab=input$attribute2, ylab="count")})
+    output$plot1 <- renderPlot({hist(data[,input$var1], main="Variable 1", xlab=input$var1, ylab="count")})
+    output$plot2 <- renderPlot({hist(data[,input$var2], main="Variable 2", xlab=input$var2, ylab="count")})
+    output$var1Desc <- renderText({paste(input$var1, ":" , description[description$var == input$var1, "desc"])})
+    output$var2Desc <- renderText({paste(input$var2, ":" , description[description$var == input$var2, "desc"])})
     
     output$lmPlot <- renderPlot({
       
       input$submitButton
-      isolate(plot(data[,input$attribute1] ~ data[,input$attribute2], main=paste(input$attribute1," vs ",input$attribute2), xlab=input$attribute2, ylab=input$attribute1))
+      isolate(plot(data[,input$var1] ~ data[,input$var2], main=paste(input$var1," vs ",input$var2), xlab=input$var2, ylab=input$var1))
     })
   }
 )
--- ui.R
+++ ui.R
@@ -3,19 +3,26 @@ shinyUI(
   pageWithSidebar(
     headerPanel("Motor Trend Car Road Tests"),
     sidebarPanel(
-      selectInput(inputId='attribute1', label='Attribute 1', choices=colnames(mtcars), selected="mpg"),
-      selectInput(inputId='attribute2', label='Attribute 2', choices=colnames(mtcars), selected="mpg"),
-      actionButton('submitButton', 'Submit'),
-      br(),
-      br(),
-      plotOutput('plot1', height="300px"),
-      br(),
-      br(),
-      plotOutput('plot2', height="300px")
+      selectInput(inputId='var1', label='Variable 1', choices=colnames(mtcars), selected="mpg"),
+      selectInput(inputId='var2', label='Variable 2', choices=colnames(mtcars), selected="mpg"),
+      actionButton('submitButton', 'Submit')
     ),
     
     mainPanel(
-      textOutput('Relationship between  and Attribute 2'),
+      
+      
+      h3('Variable Description'),
+      textOutput('var1Desc'),
+      textOutput('var2Desc'),
+      br(),
+      
+      h3('Data Distribution'),
+      splitLayout(cellWidths=300,
+                  plotOutput('plot1', height="300px"),
+                  plotOutput('plot2', height="300px")
+      ),
+      
+      h3('Relationship between variables'),
       plotOutput('lmPlot')
     )
   )
